c65976343a81f61d230a65d407f5ed6a98a6f358
DvrDania <dvr.dania@gmail.com>
2021-12-05 11:40:38 +0330

    api response class

---
+++ src/api_response.ts
@@ -0,0 +1,10 @@
+export default class apiResponse {
+  success: boolean;
+  message: string;
+  data: any;
+  constructor(success: boolean, message: string, data: any) {
+    this.success = success;
+    this.message = message;
+    this.data = data;
+  }
+}
